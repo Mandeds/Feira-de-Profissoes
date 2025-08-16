035f95878ad52bca018f5be310fa21d10dc40dfe
Calanguinho <ra45012145895@acaonsfatima.org.br>
2025-08-16 15:15:02 -0300

    a

--- src/react/home/home.jsx
+++ src/react/home/home.jsx
@@ -1,48 +1,51 @@
 import "./style.scss";
-import "../global.scss"
-import { Link } from 'react-router';
+import "../global.scss";
+import { Link } from "react-router";
 
-
-export default function home() {
+export default function Home() {
     return (
         <div className="tudo">
-
             <main>
-                <div className="vemprofrei">
-                    <h1>#VEMPROFREI</h1>
-                </div>
+                <header className="header_inicio">
+                    <div id="quadrado_inicio">
+                        <div className="vemprofrei">
+                            <h1>#VEMPROFREI</h1>
+                        </div>
 
+                        <div className="links">
+                            <strong><a href="#">INICIO</a></strong>
+                            <strong><a href="#">SOBRE</a></strong>
+                            <strong><a href="#">CONTATO</a></strong>
+                        </div>
 
-                <div id='quadrado_inicio'>
-                    
-                    <div className="links">
-                        <strong><a href="#">INICIO</a></strong>
-                        <strong><a href="#">SOBRE</a></strong>
-                        <strong><a href="#">CONTATO</a></strong>
-                    </div>
-                    
-                    <div id='quadrado_pequeno'>
-                        <div id='quadrado_borda' >
-                            <img id='imagens_inicio' src="src/assets/images/WhatsApp_Image_2025-07-29_at_16-removebg-preview 2.png" alt="100px" />
+                        <div id="quadrado_pequeno">
+                            <div id="quadrado_borda">
+                                <img
+                                    id="imagens_inicio"
+                                    src="src/assets/images/WhatsApp_Image_2025-07-29_at_16-removebg-preview 2.png"
+                                    alt="Ícone"
+                                />
+                            </div>
+                        </div>
+
+                        <div id="maior">
+                            <img
+                                id="Logo_inicio"
+                                src="src/assets/images/Group6.png"
+                                width="450px"
+                                alt="Logo"
+                            />
                         </div>
                     </div>
 
-                    <div id="maior">
-                        <img src="src/assets/images/Group6.png"/>
+                    <div className="botao">
+                        <Link to={"/cadastro"}>Cadastro</Link>
+                        <Link to={"/login"}>É O LOGAS</Link>
                     </div>
-                    
-                </div>
+                </header>
 
-                               
-                <button className="botao">  
-                    <Link to={'/cadastro'}>Cadastro</Link >
-                    <Link to={'/login'} > É O LOGAS</Link>
-                </button>
-                
-                <section className="pagina">
-
-                </section>
+                <section className="pagina"></section>
             </main>
         </div>
-    )
+    );
 }
