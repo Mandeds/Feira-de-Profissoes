3eb8e7b28656dae08c83730345bf3da186a2b08f
LGR2208 <ra43369080877@acaonsfatima.org.br>
2025-08-19 11:51:18 -0300

    SITE FEITO COM SUCESSO

--- src/react/home/home.jsx
+++ src/react/home/home.jsx
@@ -19,27 +19,52 @@ export default function Home() {
             <main className="container_main">
 
                 <section className="container_principal">
-                    <img src="/src/assets/images/Group6.png" alt="" />
+                    <img className="feira" src="/src/assets/images/Group6.png" alt="" />
+        <br />
+                    <div className="botoes">
+                        <button>
+                            <strong>CADASTRO</strong>
+                        </button>
+                        
+                        <button>
+                            <strong>LOGIN</strong>
+                        </button>
+                    </div>
+            
+                    <h1 className="horario" >20:00:00:00</h1>
 
-                    <button>CADASTRO</button>
-                    <button>LOGIN</button>
+                    <div className="carrossel">
+                        <img src="" />
+                        <img src="" />
+                        <img src="" />
+                        <img src="" />
+                    </div>
 
-                    <h1></h1>
-
-                    <div className="carrossel"></div>
-
-                    <h3></h3>
-
-                    {/* AO SELECIONAR OS ANDARES A IMAGEM AO LADO TRCA DE ACORDO COM A OPÇÂO SELECIONADA*/}
-                    <Link>ANDARES</Link>
-                    
-                </section>
+                    <div className="informacoes">
+                        <h3>VEJA NOSSAS OUTRAS PLATAFORMAS</h3>
+                        {/* AO SELECIONAR OS ANDARES A IMAGEM AO LADO TRCA DE ACORDO COM A OPÇÂO*/}
+                        <Link className="andares" >ANDARES</Link>
+                    </div>
+                </section> 
 
 
                  {/* separação de conteúdos */}
 
                 <section className="container_secundaria">
-                    
+                    <div className="card_instituto">
+                        <div className="card_interno">
+                            <h1>INSTITUTO</h1>
+                        </div>
+                    </div>
+
+                    <div className="cards_andares">
+                        <h2>Esforço que transforma 
+                    e orgulho permanece</h2>
+                        <img src="/src/assets/images/freibg.png" />
+                        <h3>PRIMEIRO ANDAR</h3>
+                        <Link to="">Curso</Link>
+                        <Link to="">Informação</Link>
+                    </div>
                 </section>
 
             </main>
